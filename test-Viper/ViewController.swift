067b23b05c6e3d94cc323b9c44d0a9e94e5d3cb5
EvgenLK <120525967+EvgenLK@users.noreply.github.com>
2024-02-18 01:02:47 +1000

    ggg

--- test-Viper/ViewController.swift
+++ test-Viper/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let one = 1
+        let two = 2
+        print(one + two)
     }
 
 
